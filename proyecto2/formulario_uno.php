416b469dedfc9d4f031c04a6cf9f84ffebcd8fe0
DanielFerreRobles <daniferrer2003@gmail.com>
2025-01-24 07:38:55 +0000

    UNO

--- proyecto2/formulario_uno.php
+++ proyecto2/formulario_uno.php
@@ -3,7 +3,7 @@
     echo '
 <form method="POST" action="index.php">
     <label for="numJugadors">Numero de jugadors:</label>
-    <input type="number" id="numJugadors" name="numJugadors" min="2" max="9" required><br><br>
+    <input type="number" id="numJugadors" name="numJugadors" min="2" max="5" required><br><br>
     
     <label for="numCartes">Numero de cartes per jugador:</label>
     <input type="number" id="numCartes" name="numCartes" value="7"><br><br>
